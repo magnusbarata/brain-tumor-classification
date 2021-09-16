e445301f876f92d7c90439adbeef73b5daab8e27
magnusbarata <barata.magnuz@gmail.com>
2021-09-17 00:19:10 +0900

    Implement ensemble_model

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,12 @@
 """Model definitions."""
 import sys
 
-# Import exposed model functions
-from models.baseline import baseline
-
 def find_model(model_name):
     """Import a model using model name"""
     this_module = sys.modules[__name__]
     return getattr(this_module, f'{model_name}')
+
+
+# Import exposed model functions
+from models.baseline import baseline
+from models.ensemble import ensemble_model
