292b38f541ad966f6a93c1ca1584a4aad93da796
magnusbarata <barata.magnuz@gmail.com>
2021-09-19 02:26:09 +0900

    Enable custom train command as option
    - Change prefix from user to hostname
    - Add read-only to mounted data

--- script/remote_train.sh
+++ script/remote_train.sh
@@ -4,7 +4,8 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 DATA_DIR='/home/kaggler/Documents/data'
-CODE_DIR='~/Documents/brain-tumor-classification-'${USER}
+CODE_DIR="~/Documents/$(hostname)_brain-tumor-classification"
+TRAIN_CMD=${1:-python train.py}
 
 echo "${GREEN}[1/3] COPYING FILES...${NC}"
 eval $(ssh-agent)
@@ -17,8 +18,8 @@ if [ $FREE_MEM -lt 1000 ]; then
     exit
 fi
 ssh kagglepc "cd ${CODE_DIR}; \
-    docker run --gpus all -u \$(id -u):\$(id -g) --name tf-gpu-${USER} \
-    --rm -v \$(pwd):/work -v ${DATA_DIR}:/work/data tf-gpu python train.py"
+    docker run --gpus all -u \$(id -u):\$(id -g) --name $(hostname)_tf-gpu \
+    --rm -v \$(pwd):/work -v ${DATA_DIR}:/work/data:ro tf-gpu ${TRAIN_CMD}"
 
 echo "${GREEN}[3/3] GET RESULTS...${NC}"
 scp -r kagglepc:${CODE_DIR}/exps/* exps/
