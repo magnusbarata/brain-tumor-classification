2ab655d4882e29dceda9b4f991425e642de0ea5b
magnusbarata <barata.magnuz@gmail.com>
2021-09-19 03:52:58 +0900

    Use trap for cleanup - Switch to bash

--- script/remote_train.sh
+++ script/remote_train.sh
@@ -1,26 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 DATA_DIR='/home/kaggler/Documents/data'
 CODE_DIR="~/Documents/$(hostname)_brain-tumor-classification"
+CONTAINER="$(hostname)_tf-gpu"
 TRAIN_CMD=${1:-python train.py}
 
-echo "${GREEN}[1/3] COPYING FILES...${NC}"
+function on_exit {
+    ssh kagglepc "rm -rf ${CODE_DIR}"
+    ssh kagglepc "docker ps -q --filter name=${CONTAINER} | grep -q . && docker stop ${CONTAINER}"
+}
+trap on_exit EXIT
+
+echo -e "${GREEN}[1/3] COPYING FILES...${NC}"
 eval $(ssh-agent)
 rsync -avz --exclude='.git' --filter=':- .gitignore' . kagglepc:${CODE_DIR}
 
-echo "${GREEN}[2/3] START TRAINING...${NC}"
+echo -e "${GREEN}[2/3] START TRAINING...${NC}"
 FREE_MEM=$(ssh kagglepc nvidia-smi --query-gpu=memory.free --format=csv,noheader,nounits)
 if [ $FREE_MEM -lt 1000 ]; then
     echo "${RED}Not enough GPU memory!${NC}"
     exit
 fi
 ssh kagglepc "cd ${CODE_DIR}; \
-    docker run --gpus all -u \$(id -u):\$(id -g) --name $(hostname)_tf-gpu \
+    docker run --gpus all -u \$(id -u):\$(id -g) --name ${CONTAINER} \
     --rm -v \$(pwd):/work -v ${DATA_DIR}:/work/data:ro tf-gpu ${TRAIN_CMD}"
 
-echo "${GREEN}[3/3] GET RESULTS...${NC}"
+echo -e "${GREEN}[3/3] GET RESULTS...${NC}"
 scp -r kagglepc:${CODE_DIR}/exps/* exps/
-ssh kagglepc "rm -rf ${CODE_DIR}/exps/*"
