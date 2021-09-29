b13ba7f2ecda4116faa44ee2fd4bcb884d6e46c7
magnusbarata <barata.magnuz@gmail.com>
2021-09-30 03:44:22 +0900

    EXP-300 - Fix efficientnet.py docstring

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -2,7 +2,17 @@ from tensorflow import keras
 import efficientnet_3D.tfkeras as efn
 
 def efficientnet(input_shape, n_class=2, variant='B0', **kwargs):
-    """EfficientNet model from the paper https://arxiv.org/abs/1905.11946"""
+    """EfficientNet model from the paper https://arxiv.org/abs/1905.11946
+    
+    Args:
+      input_shape: Shape of the input tensor, not including the `batch_size` dimension.
+      n_class: The number of class to be predicted.
+      variant: The variant of efficient net. Can be `B0`~`B7` for both 2D and 3D.
+        `L2` is available for 3D version only.
+    
+    Returns:
+      A `keras.Model` instance.
+    """
     default_effnet_params = {
         'include_top': True,
         'weights': None,
