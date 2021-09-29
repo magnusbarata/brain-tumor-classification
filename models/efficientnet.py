c4ee7d7457208088ef3a39e3f3b9778706ef9d2e
Calvin Janitra Halim <corronade@gmail.com>
2021-09-29 22:18:07 +0900

    Add try expect

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -10,16 +10,20 @@ def efficientnet(input_shape, n_class=2, variant='B0', **kwargs):
         'pooling': None,
         'classes': n_class,
     }
-    if len(input_shape) == 4:
-        effnet_layer = getattr(efn, f'EfficientNet{variant}')(**default_effnet_params)
-        modal = '3D'
-    elif len(input_shape) == 3:
-        effnet_layer = getattr(keras.applications, f'EfficientNet{variant}')(
-            classifier_activation='softmax', **default_effnet_params
-        )
-        modal = '2D'
-    else:
-        raise ValueError('input_shape is expected as an array ranked 3 or 4')
+    try:
+        if len(input_shape) == 4:
+            effnet_layer = getattr(efn, f'EfficientNet{variant}')(**default_effnet_params)
+            modal = '3D'
+        elif len(input_shape) == 3:
+            effnet_layer = getattr(keras.applications, f'EfficientNet{variant}')(
+                classifier_activation='softmax', **default_effnet_params
+            )
+            modal = '2D'
+        else:
+            raise ValueError('input_shape is expected as an array ranked 3 or 4')
+    except AttributeError:
+        print(f'No EfficientNet variation found for {variant}')
+        raise SystemExit
 
     inputs = keras.Input(input_shape)
     outputs = effnet_layer(inputs)
